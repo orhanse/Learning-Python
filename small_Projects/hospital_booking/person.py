797ff713d60dda38a1ec45f663b6cb188f258a36
orhanse <selmanorhn@gmail.com>
2025-12-09 00:29:27 +0100

    Removing old hospital_booking files

--- small_Projects/hospital_booking/person.py
+++
@@ -1,7 +0,0 @@
-class Person:
-    def __init__(self, name: str, pid: str):
-        self.name = name
-        self.id = pid
-    
-    def __str__ (self):
-        return f'{self.name} (ID: {self.id})'
