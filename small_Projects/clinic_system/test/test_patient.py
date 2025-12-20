5229af129edfbae3360250b6386ed3548a5b74db
orhanse <selmanorhn@gmail.com>
2025-12-20 12:43:09 +0100

    Fix patient test based of the new patient class

--- small_Projects/clinic_system/test/test_patient.py
+++ small_Projects/clinic_system/test/test_patient.py
@@ -6,20 +6,20 @@ from src.appointment import Appointment
 patientOne = Patient('Selman', '30', 'Kolit')
 patientTwo = Patient('Ali', '28', 'Cancer')
 
-appoinment = Appointment(patientOne.pid, '42', '2025-01-01')
+appoinment = Appointment(patientOne.pid, '42')
 
 
 def testPatientInitialization():
     assert patientOne.name == 'Selman'
     assert patientOne.pid == '30'
     assert patientOne.illness == 'Kolit'
-    assert hasattr(patientOne, 'appointments')
-    assert patientOne.appointments == {}
+    assert hasattr(patientOne, 'myAppointments')
+    assert patientOne.myAppointments == []
 
 
 def testAddAppointment():
-    patientOne.addAppointment(appoinment)
-    assert len(patientOne.appointments) == 1
+    patientOne.addDoctor(appoinment)
+    assert len(patientOne.myAppointments) == 1
 
 
 def test_patient_str():
